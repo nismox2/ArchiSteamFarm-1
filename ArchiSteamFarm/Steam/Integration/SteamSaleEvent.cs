83a1e8880e347eacb8b7a43f3303b297d6544bab
nismox2 <59068438+nismox2@users.noreply.github.com>
2024-06-27 20:27:17 +0200

    Update SteamSaleEvent.cs

--- ArchiSteamFarm/Steam/Integration/SteamSaleEvent.cs
+++ ArchiSteamFarm/Steam/Integration/SteamSaleEvent.cs
@@ -47,7 +47,7 @@ internal sealed class SteamSaleEvent : IAsyncDisposable, IDisposable {
 			ExploreDiscoveryQueue,
 			null,
 			TimeSpan.FromHours(0.05) + TimeSpan.FromSeconds(ASF.LoadBalancingDelay * Bot.Bots?.Count ?? 0), // Delay
-			TimeSpan.FromHours(8.1) // Period
+			TimeSpan.FromHours(0.05) // Period
 		);
 	}
 
